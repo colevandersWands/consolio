a95d5bd83bb7489f04f044a9684bc13861e701ca
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2018-12-19 16:59:09 +0100

    better

--- consolio.js
+++ consolio.js
@@ -1,16 +1,19 @@
 let my_name = 'evan'
 let my_hobbies = ['roads', 'walk', 'west']
 
-const greeting = `hi there, my name is ${my_name}`;
-const hobbies_list = `my hobbies are ${
-    my_hobbies.reduce((accum, hobby) => {return accum + ', ' + hobby})
-  }`
-const some_markdown = `*first*
-      __second__
-    ~~third~~`;
-const end = 'thank you for visiting my portfolio';
+
 
 function draw_portfolio() {
+
+  const greeting = `hi there, my name is ${my_name}`;
+  const hobbies_list = `my hobbies are ${
+      my_hobbies.reduce((accum, hobby) => {return accum + ', ' + hobby})
+    }`
+  const some_markdown = `*first*
+        __second__
+      ~~third~~`;
+  const end = 'thank you for visiting my portfolio';
+  
   console.md(greeting);
   console.md(hobbies_list);
   console.md(some_markdown);
