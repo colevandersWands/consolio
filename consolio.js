7271462b1a0f5aacee31bb0333883c1c9436acd5
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2018-12-19 16:57:19 +0100

    better

--- consolio.js
+++ consolio.js
@@ -1,20 +1,28 @@
 let my_name = 'evan'
 let my_hobbies = ['roads', 'walk', 'west']
 
-function draw_portfolio() {
-  console.md(`hi there, my name is ${my_name}`);
-  //  learningful unexpected behavior
-  // console.log(`my hobbies are ${my_hobbies.map(
-  //       (hobby) => {console.log(hobby + ', ')}
-  //     )}`);
-  //  or with reduce
-  console.md(`my hobbies are ${my_hobbies.reduce(
-        (accum, hobby) => {return accum + ', ' + hobby}
-      )}`);
-  console.md(`*first*
+const greeting = `hi there, my name is ${my_name}`;
+const hobbies_list = `my hobbies are ${
+    my_hobbies.reduce((accum, hobby) => {return accum + ', ' + hobby})
+  }`
+const some_markdown = `*first*
       __second__
-    ~~third~~`);
-  console.md('thank you for visiting my portfolio')
+    ~~third~~`;
+const end = 'thank you for visiting my portfolio';
+
+function draw_portfolio() {
+  console.md(greeting);
+  console.md(hobbies_list);
+  console.md(some_markdown);
+  console.md(end)
 }
 
 draw_portfolio()
+
+
+
+//  learningful unexpected behavior
+// console.log(`my hobbies are ${my_hobbies.map(
+//       (hobby) => {console.log(hobby + ', ')}
+//     )}`);
+//  or with reduce
